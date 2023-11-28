345164fbe9a83f383f75d445b5fa1d960d127a6e
Anurag Chemban <chembananurag23@gmail.com>
2023-11-28 16:58:40 +0530

    Resource Account: offer rotation & signer cap and retrieve signer cap

--- RotateResourceAccountAuthKey/aptos-rotation/index.ts
+++ RotateResourceAccountAuthKey/aptos-rotation/index.ts
@@ -17,25 +17,28 @@ const client = new AptosClient(provider.aptosClient.nodeUrl);
 
 const ACCOUNT1_PRIVATE_KEY: MaybeHexString = process.env.ACCOUNT1_PRIVATE_KEY!;
 const ACCOUNT2_PRIVATE_KEY: MaybeHexString = process.env.ACCOUNT2_PRIVATE_KEY!;
+const ACCOUNT3_PRIVATE_KEY: MaybeHexString = process.env.ACCOUNT3_PRIVATE_KEY!;
 
 const hexString: Uint8Array =
   HexString.ensure(ACCOUNT1_PRIVATE_KEY).toUint8Array();
 const hexString2: Uint8Array =
   HexString.ensure(ACCOUNT2_PRIVATE_KEY).toUint8Array();
+const hexString3: Uint8Array =
+  HexString.ensure(ACCOUNT2_PRIVATE_KEY).toUint8Array();
 
 const sender = new AptosAccount(
-  // HexString.ensure(
-  //   "0x85d044a5871b27208ea39ee519fc7393dbfd07bbcafa46e1bbde2ee5f290de23"
-  // ).toUint8Array(),
-  hexString
-);
-console.log(sender);
-console.log(
-  "publickey ",
-  Buffer.from(sender.signingKey.publicKey).toString("hex")
+  hexString,
+  "0xf63c200b4f2ecd34d20231290ca8a927fe04e7b84a254985c22482c653a8af0b" // resource_account_address, fund resource account address for tx fee.
 );
 
-const receiver = new AptosAccount(hexString2);
+const account = new AptosAccount(hexString);
+
+const receiver = new AptosAccount(hexString3);
+
+const rotate_sender = new AptosAccount(
+  hexString2,
+  "0x92f124c07912d7f502f56fdbda864b9cdad6432c1a989476d95f38feb0e95dd2" // [RESOURCE ACCOUNT ADDRESS]
+);
 
 class SignerCapabilityOfferProofChallengeV2 {
   public readonly moduleAddress: TxnBuilderTypes.AccountAddress =
@@ -89,9 +92,9 @@ class RotationCapabilityOfferProofChallengeV2 {
 const rotationProofChallenge = async () => {
   const proofChallenge = new RotationCapabilityOfferProofChallengeV2(
     await client.getChainId(),
-    Number((await client.getAccount(sender.address())).sequence_number),
-    TxnBuilderTypes.AccountAddress.fromHex(sender.address()),
-    TxnBuilderTypes.AccountAddress.fromHex(receiver.address())
+    Number((await client.getAccount(rotate_sender.address())).sequence_number),
+    TxnBuilderTypes.AccountAddress.fromHex(rotate_sender.address()),
+    TxnBuilderTypes.AccountAddress.fromHex(account.address())
   );
 
   return proofChallenge;
@@ -101,70 +104,95 @@ const signerCapProofChallenge = async () => {
   const proofChallenge = new SignerCapabilityOfferProofChallengeV2(
     Number((await client.getAccount(sender.address())).sequence_number),
     TxnBuilderTypes.AccountAddress.fromHex(sender.address()),
-    TxnBuilderTypes.AccountAddress.fromHex(sender.address())
+    TxnBuilderTypes.AccountAddress.fromHex(account.address())
   );
 
   return proofChallenge;
 };
 
-const signStructAndSubmitTransaction = async (
-  provider: Provider,
-  signer: AptosAccount,
-  struct:
-    | SignerCapabilityOfferProofChallengeV2
-    | RotationCapabilityOfferProofChallengeV2,
-  accountScheme: number = 0
-): Promise<any> => {
-  const bcsStruct = BCS.bcsToBytes(struct);
-  const signedMessage = signer.signBuffer(bcsStruct);
+const transaction = async (sender: AptosAccount, payload: any) => {
+  const txn = await client.generateTransaction(sender.address(), payload);
 
-  const payload = new TxnBuilderTypes.TransactionPayloadEntryFunction(
-    TxnBuilderTypes.EntryFunction.natural(
-      `${struct.moduleAddress.toHexString()}::${struct.moduleName}`,
-      struct.functionName,
-      [],
-      [
-        BCS.bcsSerializeBytes(signedMessage.toUint8Array()),
-        BCS.bcsSerializeU8(accountScheme),
-        BCS.bcsSerializeBytes(signer.pubKey().toUint8Array()),
-        BCS.bcsToBytes(struct.recipientAddress),
-      ]
-    )
-  );
-  const txnResponse = await provider.generateSignSubmitWaitForTransaction(
-    signer,
-    payload
-  );
-  return txnResponse;
+  const signTx = await client.signTransaction(sender, txn);
+
+  try {
+    const tx = await client.submitTransaction(signTx);
+    console.log(tx);
+  } catch {
+    console.log("error");
+  }
 };
 
-(async () => {
+const offerSignerCap = async () => {
   const signerProofChallenge = await signerCapProofChallenge();
 
-  const rotateProofChallenge = await rotationProofChallenge();
-
   const signerStruct = BCS.bcsToBytes(signerProofChallenge);
-  const signerCapSignedMessage = sender.signBuffer(signerStruct);
+  // console.log(signerStruct);
+  // const signerCapSignedMessage = sender.signBuffer(signerStruct);
+
+  const message = Buffer.from(signerStruct).toString("hex");
+  const signerCapSignedMessage = sender.signHexString(message);
 
   console.log(
     "SignerCapabilityOfferProofChallengeV2 signature: ",
     signerCapSignedMessage
   );
 
+  const offer_signer_cap_payload = {
+    type: "entry_function_payload",
+    function:
+      "0x679f214e1a1e0b4341f55990c0ff2a514a63cf5a9a5b76c66ff6ac195f0fa220::rotate::offer_signer_cap",
+    type_arguments: [],
+    arguments: [account.address(), signerCapSignedMessage, sender.pubKey()],
+  };
+
+  transaction(sender, offer_signer_cap_payload);
+};
+
+const rotationCap = async () => {
+  const rotateProofChallenge = await rotationProofChallenge();
+
   const rotationStruct = BCS.bcsToBytes(rotateProofChallenge);
-  const rotateSignedMessage = sender.signBuffer(rotationStruct);
+  // console.log(rotationStruct);
+  // const rotationCapSignedMessage = rotate_sender.signBuffer(rotationStruct);
+
+  const message = Buffer.from(rotationStruct).toString("hex");
+  const rotationCapSignedMessage = rotate_sender.signHexString(message);
 
   console.log(
     "RotationCapabilityOfferProofChallengeV2 signature: ",
-    rotateSignedMessage
+    rotationCapSignedMessage
   );
 
-  console.log(
-    await signStructAndSubmitTransaction(
-      provider,
-      sender,
-      signerProofChallenge,
-      0
-    )
-  );
+  const rotation_cap_payload = {
+    type: "entry_function_payload",
+    function:
+      "0x679f214e1a1e0b4341f55990c0ff2a514a63cf5a9a5b76c66ff6ac195f0fa220::rotate::offer_rotation_cap",
+    type_arguments: [],
+    arguments: [
+      account.address(),
+      rotationCapSignedMessage,
+      rotate_sender.pubKey(),
+    ],
+  };
+
+  transaction(rotate_sender, rotation_cap_payload);
+};
+
+const retrieveSignerCap = async () => {
+  const retrieve_signer_cap_payload = {
+    type: "entry_function_payload",
+    function:
+      "0x679f214e1a1e0b4341f55990c0ff2a514a63cf5a9a5b76c66ff6ac195f0fa220::rotate::retrive_signer_cap",
+    type_arguments: [],
+    arguments: [],
+  };
+
+  transaction(account, retrieve_signer_cap_payload);
+};
+
+(async () => {
+  await offerSignerCap();
+  await rotationCap();
+  await retrieveSignerCap();
 })();
--- RotateResourceAccountAuthKey/sources/rotate_auth_key.move
+++ RotateResourceAccountAuthKey/sources/rotate_auth_key.move
@@ -24,9 +24,9 @@ module addr::rotate {
 
     // This function utilize create_resouce_account method from resource_account module in aptos_framework
     // It create a resource account and set the auth_key to the provided key.
-    public entry fun create_resource_account(account: &signer, auth_key: vector<u8>) acquires ResourceAccountData {
-        resource_account::create_resource_account(account, x"1001", auth_key);
-        let resource_account_address = account::create_resource_address(&signer::address_of(account), x"1001");
+    public entry fun create_resource_account(account: &signer, auth_key: vector<u8>, seed: vector<u8>) acquires ResourceAccountData {
+        resource_account::create_resource_account(account, seed, auth_key);
+        let resource_account_address = account::create_resource_address(&signer::address_of(account), seed);
 
         let resource_account_data = borrow_global_mut<ResourceAccountData>(@addr);
         table::add(&mut resource_account_data.resource_account, signer::address_of(account), resource_account_address); 
@@ -34,8 +34,8 @@ module addr::rotate {
 
     // This function utilize create_resource_account method from account mdoule in aptos_framework
     // It creates the resource account and claims signer_cap and set auth_key to 0x0
-    public entry fun create_resource_account_and_claim_signer_cap(account: &signer) acquires ResourceAccountData, SignerCap {
-        let (resource_account_signer, resource_signer_cap) = account::create_resource_account(account, x"1001");
+    public entry fun create_resource_account_and_claim_signer_cap(account: &signer, seed: vector<u8>) acquires ResourceAccountData, SignerCap {
+        let (resource_account_signer, resource_signer_cap) = account::create_resource_account(account, seed);
         let resource_account_address = signer::address_of(&resource_account_signer);
 
         let resource_account_data = borrow_global_mut<ResourceAccountData>(@addr);
@@ -45,25 +45,24 @@ module addr::rotate {
         table::add(&mut resource_signer_cap_data.cap, resource_account_address, resource_signer_cap);
     }
 
-    public entry fun offer_signer_cap(account: &signer, addr: address, signer_capability_sig_bytes: vector<u8>, account_public_key_bytes: vector<u8>) {
-        // let resource_account_data = borrow_global<ResourceAccountData>(@addr);
-        // let resource_address = *table::borrow(&resource_account_data.resource_account, signer::address_of(account));
+    public entry fun offer_signer_cap(account: &signer, recipient_addr: address, signer_capability_sig_bytes: vector<u8>, account_public_key_bytes: vector<u8>) {
+        account::offer_signer_capability(account, signer_capability_sig_bytes, 0, account_public_key_bytes, recipient_addr);
+    }
 
-        // let resource_signer_cap_data = borrow_global_mut<SignerCap>(@addr);
-        // let resource_signer = table::borrow(&mut resource_signer_cap_data.cap, resource_address);
-
-        account::offer_signer_capability(account, signer_capability_sig_bytes, 0, account_public_key_bytes, addr);
+    public entry fun offer_rotation_cap(account: &signer, recipient_addr: address, rotation_capability_sig_bytes: vector<u8>, account_public_key_bytes: vector<u8>) {
+        account::offer_rotation_capability(account, rotation_capability_sig_bytes, 0, account_public_key_bytes, recipient_addr);
     }
 
     public entry fun retrive_signer_cap(account: &signer) acquires ResourceAccountData, SignerCap {
+        let source_addr = signer::address_of(account);
         let resource_account_data = borrow_global<ResourceAccountData>(@addr);
-        let resource_address = *table::borrow(&resource_account_data.resource_account, signer::address_of(account));
+        let resource_address = *table::borrow(&resource_account_data.resource_account, source_addr);
 
         let resource_signer_cap_data = borrow_global_mut<SignerCap>(@addr);
 
         let resource_signer = account::create_authorized_signer(account, resource_address);
 
-        let signer_cap = resource_account::retrieve_resource_account_cap(&resource_signer, resource_address);
+        let signer_cap = resource_account::retrieve_resource_account_cap(&resource_signer, source_addr);
         table::add(&mut resource_signer_cap_data.cap, resource_address, signer_cap);
     }
 
